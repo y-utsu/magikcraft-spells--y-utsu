974cda59da14848d94b8de221a19f7c659cee827
y-utsu <yoshikazu.utsu@gmail.com>
2017-11-07 14:38:55 +0000

    Update teleport.js

--- teleport.js
+++ teleport.js
@@ -4,8 +4,5 @@ function teleport() {
     const there = magik.memento.getItem();
     if (there) {
         magik.ianuae(there);
-    } else {
-        const here = magik.hic();
-        magik.memento(here);
     }
 }
