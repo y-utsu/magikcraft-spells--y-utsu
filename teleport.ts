9c0f43db4ebac3c58fa9b89dfd25696d2fc0d694
y-utsu <yoshikazu.utsu@gmail.com>
2017-11-07 16:04:36 +0000

    Update teleport.ts

--- teleport.ts
+++ teleport.ts
@@ -1,6 +1,6 @@
 const magik = magikcraft.io;
 
-function teleport3(placeName) {
+function teleport(placeName) {
     const there = magik.memento.getItem(placeName);
     if (there) {
         magik.ianuae(there);
