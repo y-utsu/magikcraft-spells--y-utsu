7944ab1d2a4d52f5d0abcab04cd1529456950256
y-utsu <yoshikazu.utsu@gmail.com>
2017-11-07 16:18:20 +0000

    Update showMemory.ts

--- showMemory.ts
+++ showMemory.ts
@@ -3,6 +3,7 @@ const magik = magikcraft.io;
 function showMemory() {
      const thing = magik.exmemento();
      for (var key in thing) {
-         magik.dixit(thing[key]);
+        magik.dixit(key);
+        magik.dixit(thing[key]);
      }
 }
