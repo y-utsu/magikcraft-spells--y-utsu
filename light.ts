1ecb759fd764d02da6e1a176e2aa7a2dcdf4ba85
y-utsu <yoshikazu.utsu@gmail.com>
2017-11-08 11:54:56 +0000

    Update light.ts

--- light.ts
+++ light.ts
@@ -1,13 +1,14 @@
 const magik = magikcraft.io;
 
 function light() {
+
     var counter = 3;
     var lit = 
         function (){
             magik.dixit("counter:"+counter);
             magik.noxvida();
             if (0 <= counter){
-                setTimeout(lit, 10000);
+                magik.setTimeout(lit, 10000);
             }
             counter--;
         };
